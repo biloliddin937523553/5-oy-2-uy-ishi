b2906fe463d115f6e869d654ae08c7f12d548fd5
biloliddin937523553 <biloliddinlatipov0@gmail.com>
2024-03-27 20:40:23 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let ism = prompt('Ismingizni kiriting')
+let id = prompt('Space idigizni kiriting')
+let pw = prompt('Space passwordingizni kiriting')
+
+let person = {
+    name : ism,
+    id : id,
+    password : pw
+}
+console.log(person);
